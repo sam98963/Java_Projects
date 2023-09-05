cd0fea1de566907d4555905422402ccd197c49f8
sam98963 <szwilliamson01@gmail.com>
2023-09-05 11:03:32 +0100

    switch case and pascal case conversion

--- TextConverter/TextConverter.java
+++ TextConverter/TextConverter.java
@@ -5,6 +5,7 @@ public class TextConverter {
   private String convertedText;
   private int converterValue;
   private Scanner scanner;
+  private StringBuilder stringBuilder;
 
 
   public TextConverter(){
@@ -12,6 +13,7 @@ public class TextConverter {
     this.convertedText = "";
     this.converterValue = 0;
     this.scanner = new Scanner(System.in);
+    this.stringBuilder = new StringBuilder("");
   }
 
   public void initialiseAppGetTextInput(){
@@ -19,39 +21,54 @@ public class TextConverter {
     System.out.print("Please enter your text\n");
     this.inputText = this.scanner.nextLine();
     System.out.print("Here are the possible text-conversion formats:\n");
-    System.out.print("1. camelCase\n 2. PascalCase\n 3. snake_case\n 4. MACRO_CASE\n 5. kebab-case\n 6. Train-Case\n");
+    System.out.print("1. camelCase\n");
     System.out.print("2. PascalCase\n");
     System.out.print("3. snake_case\n");
     System.out.print("4. MACRO_CASE\n");
     System.out.print("5. kebab-case\n");
     System.out.print("6. Train-Case\n");   
+    System.out.print("7. mEme cASe\n");   
     System.out.print("Which would you like, please enter a number.\n");
     this.converterValue = this.scanner.nextInt();
   }
 
 
   public void validateConversionInput (){
-    if(this.converterValue < 3){
-      convertCamelCase();
-    } if (this.converterValue > 2 && this.converterValue < 5){
-      // Call "_" joined function
-    } else {
-      // Call "-" joined function
-    }
+      switch(this.converterValue){
+        case 1: convertCamelCase();
+        break;
+        
+        case 2: convertPascalCase();
+        break;
+      }
   }
 
   public void convertCamelCase(){
     String[] textSplit = this.inputText.split(" ");
 
-    StringBuilder camelCaseText = new StringBuilder(textSplit[0]);
+    this.stringBuilder.append(textSplit[0]);
     for(int i = 1; i<textSplit.length; i++){
       String currentWord = textSplit[i];
       String currentWordCapitalised = Character.toUpperCase(currentWord.charAt(0)) + currentWord.substring(1);
-      camelCaseText.append(currentWordCapitalised);
+      this.stringBuilder.append(currentWordCapitalised);
     }
-    this.convertedText = camelCaseText.toString();
+    this.convertedText = this.stringBuilder.toString();
     System.out.print(this.convertedText);
   }
+
+  public void convertPascalCase(){
+    String[] textSplit = this.inputText.split(" ");
+
+    for(int i = 0; i<textSplit.length; i++){
+      String currentWord = textSplit[i];
+      String currentWordCapitalised = Character.toUpperCase(currentWord.charAt(0)) + currentWord.substring(1);
+      this.stringBuilder.append(currentWordCapitalised);
+    }
+    this.convertedText = this.stringBuilder.toString();
+    System.out.print(this.convertedText);
+  }
+
+
 
   public static void main(String[] args) {
     TextConverter converter = new TextConverter();
